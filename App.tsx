524c922beff1eb62fabdd85bcdd0d9999d2125cb
Paulo Lima <prmlimajr@hotmail.com>
2021-12-22 00:31:05 -0300

    Add theme global styles

--- App.tsx
+++ App.tsx
@@ -1,5 +1,22 @@
 import { Home } from './src/screens/Home';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import {
+  Lato_400Regular,
+  Lato_700Bold,
+  Lato_900Black,
+} from '@expo-google-fonts/lato';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+    Lato_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Home />;
 }
---
+++ src/global/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    primary100: '#333333',
+    primary80: '#25282B',
+    primary50: '#828282',
+    primary20: '#E5E5E5',
+
+    secondary100: '#FF9900',
+  },
+
+  fonts: {
+    title900: 'Lato_900Black',
+    title700: 'Lato_700Bold',
+    text400: 'Lato_400Regular',
+  },
+};
